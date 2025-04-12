f398a91b7c7a4bd7595102815f6490641a0270e8
Nicolas Gabriel Martins <nicolasgabrielmartinssilva@gmail.com>
2025-04-12 14:43:39 -0300

    Sistema de cadastro e login, usando JWT

--- nba_connect/settings.py
+++ nba_connect/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'players',
+    'stats',
+    'teams',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -115,9 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
